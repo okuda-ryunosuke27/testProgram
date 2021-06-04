390e9c45ce128ca8f4a5d49d85afe48527e8a090
okuda-ryunosuke27 <83803851+okuda-ryunosuke27@users.noreply.github.com>
2021-06-04 21:00:49 +0900

    構造体で残りの敵と弾の関数を作った

--- test/test/main.cpp
+++ test/test/main.cpp
@@ -27,24 +27,41 @@ typedef struct {
 	int isEnemyAlive;
 	int enemyTymer;
 }Enemy2;
+typedef struct {
+	int bulX;
+	int bulY;
+	int bulR;
+	int isbulFlag;
+}Bullet;
+typedef struct {
+	int bulX;
+	int bulY;
+	int bulR;
+	int isbulFlag;
+}Bullet2;
+typedef struct {
+	int bulX;
+	int bulY;
+	int bulR;
+	int isbulFlag;
+}Bullet3;
 Player play;
 Enemy enemy;
 Enemy2 enemy2;
+Bullet bul;
+Bullet2 bul2;
+Bullet3 bul3;
 
 
-int Enemy3X = -20, Enemy3Y = 100, Enemy3R = 16;
-int E_getpos, E_getpos2, E_getpos3; //isEnemyAlive = 1, enemyTymer = 120;
-int E_getpos4, E_getpos5, E_getpos6; //isEnemyAlive2 = 1, enemyTymer2 = 120;
+int E_getpos, E_getpos2, E_getpos3;
+int E_getpos4, E_getpos5, E_getpos6;
 int Speed = 2, Speed2 = 2, Speed3 = 2;
 int enemycount = 0;
-
-int bulx = -10, buly = -10, bulr = 10, isbulFlag = 0;
-int bul2x = -10, bul2y = -10, bul2r = 10, isbulFlag2 = 0;
-int bul3x = -10, bul3y = -10, bul3r = 10, isbulFlag3 = 0;
 
 int bgX = 0, bgY = 0;
 int bg2X = 0, bg2Y = -400;
 int rad;
+int scene = 0;
 
 //最新のキーボード情報用
 char keys[256] = { 0 };
@@ -53,8 +70,10 @@ char oldkeys[256] = { 0 };
 
 void (*pl)();
 void (*en)();
+void (*bu)();
 void PlayerUpdata();
 void EnemyUpdata();
+void BulletUpdata();
 void Initialize();
 // プログラムは WinMain から始まります
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
@@ -110,273 +129,102 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 		ClearDrawScreen();
 		
 		//更新処理
-		pl = PlayerUpdata;
-		en = EnemyUpdata;
-		(*pl)();
-		(*en)();
-		//弾の処理
-		if (keys[KEY_INPUT_SPACE] == 1 && oldkeys[KEY_INPUT_SPACE] == 0)
+		if (scene == 0)
 		{
-			if (isbulFlag == 0 || isbulFlag2 ==0 || isbulFlag == 0)
+			if (keys[KEY_INPUT_RETURN] == 1 && oldkeys[KEY_INPUT_RETURN] == 0)
 			{
-				bulx = play.PlayerX + 15;
-				buly = play.PlayerY + 15;
-
-				bul2x = play.PlayerX + 15;
-				bul2y = play.PlayerY + 15;
-				
-				bul3x = play.PlayerX + 15;
-				bul3y = play.PlayerY + 15;
-
-				isbulFlag = 1;
-				isbulFlag2 = 1;
-				isbulFlag3 = 1;
+				scene = 1;
 			}
 		}
-
-		if (isbulFlag == 1)
+		if (scene == 1)
 		{
-			buly -= 10;
+			pl = PlayerUpdata;
+			en = EnemyUpdata;
+			bu = BulletUpdata;
+			(*pl)();
+			(*en)();
+			(*bu)();
+			//弾の処理
 
-			if (buly < -20)
+
+			//パックマン壁端移動-
+			/*if (PlayerX > 600)
 			{
-				isbulFlag = 0;
+				PlayerX = -32;
 			}
-		}
-		if (isbulFlag2 == 1)
-		{
-			bul2x -= 5;
-			bul2y -= 10;
-			if (bul2y < -20)
+			if (PlayerX + 32 < 0)
 			{
-				isbulFlag2 = 0;
+				PlayerX = 595;
 			}
-		} 
-		if (isbulFlag3 == 1)
-		{
-			bul3x += 5;
-			bul3y -= 10;
-			if (bul3y < -20)
+			if (PlayerY + 32 < 0)
 			{
-				isbulFlag3 = 0;
+				PlayerY = 395;
 			}
-		}
-		E_getpos = sqrt((enemy.EnemyX - bulx) * (enemy.EnemyX - bulx) + (enemy.EnemyY - buly) * (enemy.EnemyY - buly));
-		E_getpos2 = sqrt((enemy.EnemyX - bul2x) * (enemy.EnemyX - bul2x) + (enemy.EnemyY - bul2y) * (enemy.EnemyY - bul2y));
-		E_getpos3 = sqrt((enemy.EnemyX - bul3x) * (enemy.EnemyX - bul3x) + (enemy.EnemyY - bul3y) * (enemy.EnemyY - bul3y));
-		if ( isbulFlag == 1 || isbulFlag2 == 1 || isbulFlag3 == 1 || enemy.isEnemyAlive == 1)
-		{
-			if (E_getpos < bulr + enemy.EnemyR)
+			if (PlayerY > 400)
 			{
-				isbulFlag = 0;
-				enemy.isEnemyAlive = 0;
-				enemy.EnemyX = -200;
-				enemy.EnemyY = -200;
-				Speed = 0;
-				enemycount += 10;
+				PlayerY = -32;
+			}*/
 
-				bulx = -100;
-				buly = -100;
-			}
-			if (E_getpos2 < bul2r + enemy.EnemyR)
+			//描画処理
+			//背景スクロール
+			DrawGraph(bgX, bgY, back1, FALSE);
+			DrawGraph(bg2X, bg2Y, back2, FALSE);
+			bgY += 2;
+			bg2Y += 2;
+			if (bgY > 400)
 			{
-				isbulFlag2 = 0;
-				enemy.isEnemyAlive = 0;
-				enemy.EnemyX = -200;
-				enemy.EnemyY = -200;
-				Speed = 0;
-				enemycount += 10;
-
-				bul2x = -100;
-				bul2y = -100;
+				bgY = -395;
 			}
-			if (E_getpos3 < bul2r + enemy.EnemyR)
+			if (bg2Y > 400)
 			{
-				isbulFlag3 = 0;
-				enemy.isEnemyAlive = 0;
-				enemy.EnemyX = -200;
-				enemy.EnemyY = -200;
-				Speed = 0;
-				enemycount += 10;
-
-				bul3x = -100;
-				bul3y = -100;
+				bg2Y = -395;
 			}
-		}
-		if (enemy.isEnemyAlive == 0)
-		{
-			enemy.enemyTymer = enemy.enemyTymer - 1;
-			if (enemy.enemyTymer < 0)
+
+			//実機,プレイヤー描画
+			DrawCircle(bul.bulX, bul.bulY, bul.bulR, GetColor(255, 255, 255), TRUE);
+			DrawCircle(bul2.bulX, bul2.bulY, bul2.bulR, GetColor(255, 255, 255), TRUE);
+			DrawCircle(bul3.bulX, bul3.bulY, bul3.bulR, GetColor(255, 255, 255), TRUE);
+
+			DrawGraph(play.PlayerX, play.PlayerY, player, TRUE);
+			//DrawBox(PlayerX, PlayerY, PlayerX + 32, PlayerY + 32, GetColor(255, 255, 255), TRUE);
+			if (enemy.isEnemyAlive == 1)
 			{
-				enemy.isEnemyAlive = 1;
-				enemy.enemyTymer = 120;
-				enemy.EnemyX = rand() % 200 + 100;
-				enemy.EnemyY = 10;
-				if (rand() % 2)
-				{
-					Speed = 2;
-				}
-				else
-				{
-					Speed = -2;
-				}
-				
+				DrawBox(enemy.EnemyX, enemy.EnemyY, enemy.EnemyX + 32, enemy.EnemyY + 32, GetColor(255, 0, 0), TRUE);
 			}
-		}
-		E_getpos4 = sqrt((enemy2.EnemyX - bulx) * (enemy2.EnemyX - bulx) + (enemy2.EnemyY - buly) * (enemy2.EnemyY - buly));
-		E_getpos5 = sqrt((enemy2.EnemyX - bul2x) * (enemy2.EnemyX - bul2x) + (enemy2.EnemyY - bul2y) * (enemy2.EnemyY - bul2y));
-		E_getpos6 = sqrt((enemy2.EnemyX - bul3x) * (enemy2.EnemyX - bul3x) + (enemy2.EnemyY - bul3y) * (enemy2.EnemyY - bul3y));
-		if (isbulFlag == 1 || isbulFlag2 == 1 || isbulFlag3 == 1 || enemy2.isEnemyAlive == 1)
-		{
-			if (E_getpos4 < bulr + enemy2.EnemyR)
+			if (enemy2.isEnemyAlive == 1)
 			{
-				isbulFlag = 0;
-				enemy2.isEnemyAlive = 0;
-				enemy2.EnemyX = -200;
-				enemy2.EnemyY = -200;
-				Speed2 = 0;
-				enemycount += 15;
-
-				bulx = -100;
-				buly = -100;
+				DrawCircle(enemy2.EnemyX, enemy2.EnemyY, enemy2.EnemyR, GetColor(255, 255, 0), TRUE);
 			}
-			if (E_getpos5 < bul2r + enemy2.EnemyR)
-			{
-				isbulFlag2 = 0;
-				enemy2.isEnemyAlive = 0;
-				enemy2.EnemyX = -200;
-				enemy2.EnemyY = -200;
-				Speed2 = 0;
-				enemycount += 15;
-
-				bul2x = -100;
-				bul2y = -100;
-			}
-			if (E_getpos6 < bul2r + enemy2.EnemyR)
-			{
-				isbulFlag3 = 0;
-				enemy2.isEnemyAlive = 0;
-				enemy2.EnemyX = -200;
-				enemy2.EnemyY = -200;
-				Speed2 = 0;
-				enemycount += 15;
-
-				bul3x = -100;
-				bul3y = -100;
-			}
+			//デバック
+			DrawBox(0, 400, 600, 600, GetColor(0, 0, 0), TRUE);
+			DrawFormatString(0, 410, GetColor(255, 255, 255),
+				"(左上 PlayerX: %3dpx, PlayerY: %3dpx)", play.PlayerX, play.PlayerY);
+			DrawFormatString(0, 430, GetColor(255, 255, 255),
+				"(右上 PlayerX: %3dpx, PlayerY: %3dpx)", play.PlayerX + 32, play.PlayerY);
+			DrawFormatString(0, 450, GetColor(255, 255, 255),
+				"(左下 PlayerX: %3dpx, PlayerY: %3dpx)", play.PlayerX, play.PlayerY + 32);
+			DrawFormatString(0, 470, GetColor(255, 255, 255),
+				"(右下 PlayerX: %3dpx, PlayerY: %3dpx)", play.PlayerX + 32, play.PlayerY + 32);
+			DrawFormatString(0, 490, GetColor(255, 255, 255),
+				"(Enemy1:X%3dpx,Y%3dpx)", enemy.EnemyX, enemy.EnemyY);
+			DrawFormatString(0, 510, GetColor(255, 255, 255),
+				"(Enemy2:X%3dpx,Y%3dpx)", enemy2.EnemyX, enemy2.EnemyY);
+			DrawFormatString(0, 530, GetColor(255, 255, 255),
+				"背景:Y%3dpx", bgY);
+			DrawFormatString(110, 530, GetColor(255, 255, 255),
+				"背景:Y%3dpx", bg2Y);
+			DrawFormatString(230, 530, GetColor(255, 255, 255),
+				"復活カウント:%3d", enemy.enemyTymer);
+			DrawFormatString(0, 550, GetColor(255, 255, 255),
+				"score:%3dpt", enemycount);
 		}
-		if (enemy2.isEnemyAlive == 0)
+		if (enemycount >= 100)
 		{
-			enemy2.enemyTymer = enemy2.enemyTymer - 1;
-			if (enemy2.enemyTymer < 0)
-			{
-				enemy2.isEnemyAlive = 1;
-				enemy2.enemyTymer = 120;
-				enemy2.EnemyX = rand() % 200 + 100;
-				enemy2.EnemyY = 100;
-				if (rand() % 2)
-				{
-					Speed2 = 2;
-				}
-				else
-				{
-					Speed2 = -2;
-				}
-			}
+			SetFontSize(20);
+			DrawFormatString(250, 300, GetColor(255, 255, 255),
+				"スコアは：%3d点", enemycount);
+			
 		}
-		//壁に当たる
-		if (play.PlayerX < 0)
-		{
-			play.PlayerX = 0;
-		}
-		if (play.PlayerX + 32 > 600)
-		{
-			play.PlayerX = 568;
-		}
-		if (play.PlayerY < 0)
-		{
-			play.PlayerY = 0;
-		}
-		if (play.PlayerY + 32 > 400)
-		{
-			play.PlayerY = 368;
-		}
-		//パックマン壁端移動-
-		/*if (PlayerX > 600)
-		{
-			PlayerX = -32;
-		}
-		if (PlayerX + 32 < 0)
-		{
-			PlayerX = 595;
-		}
-		if (PlayerY + 32 < 0)
-		{
-			PlayerY = 395;
-		}
-		if (PlayerY > 400)
-		{
-			PlayerY = -32;
-		}*/
-
-		//敵の当たり判定
-		
-
-		//敵の行動(レッド)
-		
-
-		//描画処理
-		//背景スクロール
-		DrawGraph(bgX, bgY, back1, FALSE);
-		DrawGraph(bg2X, bg2Y, back2, FALSE);
-		bgY += 2;
-		bg2Y += 2;
-		if (bgY > 400)
-		{
-			bgY = -395;
-		}
-		if (bg2Y > 400)
-		{
-			bg2Y = -395;
-		}
-		
-		//実機,プレイヤー描画
-		DrawCircle(bulx, buly, bulr, GetColor(255, 255, 255), TRUE);
-		DrawCircle(bul2x, bul2y, bul2r, GetColor(255, 255, 255), TRUE);
-		DrawCircle(bul3x, bul3y, bul3r, GetColor(255, 255, 255), TRUE);
-
-		DrawGraph(play.PlayerX, play.PlayerY,player,TRUE);
-		//DrawBox(PlayerX, PlayerY, PlayerX + 32, PlayerY + 32, GetColor(255, 255, 255), TRUE);
-		if (enemy.isEnemyAlive == 1)
-		{
-			DrawBox(enemy.EnemyX, enemy.EnemyY, enemy.EnemyX + 32, enemy.EnemyY + 32, GetColor(255, 0, 0), TRUE);
-		}
-		if (enemy2.isEnemyAlive == 1)
-		{
-			DrawCircle(enemy2.EnemyX, enemy2.EnemyY, enemy2.EnemyR, GetColor(255, 255, 0), TRUE);
-		}
-		//デバック
-		DrawBox(0, 400, 600, 600, GetColor(0, 0, 0), TRUE);
-		DrawFormatString(0, 410, GetColor(255, 255, 255),
-			"(左上 PlayerX: %3dpx, PlayerY: %3dpx)", play.PlayerX, play.PlayerY);
-		DrawFormatString(0, 430, GetColor(255, 255, 255),
-			"(右上 PlayerX: %3dpx, PlayerY: %3dpx)", play.PlayerX + 32, play.PlayerY);
-		DrawFormatString(0, 450, GetColor(255, 255, 255),
-			"(左下 PlayerX: %3dpx, PlayerY: %3dpx)", play.PlayerX, play.PlayerY + 32);
-		DrawFormatString(0, 470, GetColor(255, 255, 255),
-			"(右下 PlayerX: %3dpx, PlayerY: %3dpx)", play.PlayerX + 32, play.PlayerY + 32);
-		DrawFormatString(0, 490, GetColor(255, 255, 255),
-			"(Enemy1:X%3dpx,Y%3dpx)", enemy.EnemyX, enemy.EnemyY);
-		DrawFormatString(0, 510, GetColor(255, 255, 255),
-			"(Enemy2:X%3dpx,Y%3dpx)", enemy2.EnemyX, enemy2.EnemyY);
-		DrawFormatString(0, 530, GetColor(255, 255, 255),
-			"背景:Y%3dpx", bgY);
-		DrawFormatString(110, 530, GetColor(255, 255, 255),
-			"背景:Y%3dpx", bg2Y);
-		DrawFormatString(230, 530, GetColor(255, 255, 255),
-			"復活カウント:%3d", enemy.enemyTymer);
-		DrawFormatString(0, 550, GetColor(255, 255, 255),
-			"score:%3dpt", enemycount);
 	
 		//(ダブルバッファ)裏面
 		ScreenFlip();
@@ -419,9 +267,28 @@ void PlayerUpdata()
 	{
 		play.PlayerX -= 5;
 	}
+
+	//壁に当たる
+	if (play.PlayerX < 0)
+	{
+		play.PlayerX = 0;
+	}
+	if (play.PlayerX + 32 > 600)
+	{
+		play.PlayerX = 568;
+	}
+	if (play.PlayerY < 0)
+	{
+		play.PlayerY = 0;
+	}
+	if (play.PlayerY + 32 > 400)
+	{
+		play.PlayerY = 368;
+	}
 }
 void EnemyUpdata()
 {
+	//敵の行動(レッド)
 	if (enemy.EnemyX + 30 >= 600 || enemy.EnemyX <= 0)
 	{
 		Speed *= -1;
@@ -435,11 +302,186 @@ void EnemyUpdata()
 	}
 	enemy2.EnemyX += Speed2;
 }
+void BulletUpdata()
+{
+	if (keys[KEY_INPUT_SPACE] == 1 && oldkeys[KEY_INPUT_SPACE] == 0)
+	{
+		if (bul.isbulFlag == 0 || bul2.isbulFlag == 0 || bul3.isbulFlag == 0)
+		{
+			bul.bulX = play.PlayerX + 15;
+			bul.bulY = play.PlayerY + 15;
 
+			bul2.bulX = play.PlayerX + 15;
+			bul2.bulY = play.PlayerY + 15;
+
+			bul3.bulX = play.PlayerX + 15;
+			bul3.bulY = play.PlayerY + 15;
+
+			bul.isbulFlag = 1;
+			bul2.isbulFlag = 1;
+			bul3.isbulFlag = 1;
+		}
+	}
+
+	if (bul.isbulFlag == 1)
+	{
+		bul.bulY -= 10;
+
+		if (bul.bulY < -20)
+		{
+			bul.isbulFlag = 0;
+		}
+	}
+	if (bul2.isbulFlag == 1)
+	{
+		bul2.bulX -= 5;
+		bul2.bulY -= 10;
+		if (bul2.bulY < -20)
+		{
+			bul2.isbulFlag = 0;
+		}
+	}
+	if (bul3.isbulFlag == 1)
+	{
+		bul3.bulX += 5;
+		bul3.bulY -= 10;
+		if (bul3.bulY < -20)
+		{
+			bul3.isbulFlag = 0;
+		}
+	}
+	E_getpos = sqrt((enemy.EnemyX - bul.bulX) * (enemy.EnemyX - bul.bulX) + (enemy.EnemyY - bul.bulY) * (enemy.EnemyY - bul.bulY));
+	E_getpos2 = sqrt((enemy.EnemyX - bul2.bulX) * (enemy.EnemyX - bul2.bulX) + (enemy.EnemyY - bul2.bulY) * (enemy.EnemyY - bul2.bulY));
+	E_getpos3 = sqrt((enemy.EnemyX - bul3.bulX) * (enemy.EnemyX - bul3.bulX) + (enemy.EnemyY - bul3.bulY) * (enemy.EnemyY - bul3.bulY));
+	if (bul.isbulFlag == 1 || bul2.isbulFlag == 1 || bul3.isbulFlag == 1 || enemy.isEnemyAlive == 1)
+	{
+		if (E_getpos < bul.bulR + enemy.EnemyR)
+		{
+			bul.isbulFlag = 0;
+			enemy.isEnemyAlive = 0;
+			enemy.EnemyX = -200;
+			enemy.EnemyY = -200;
+			Speed = 0;
+			enemycount += 10;
+
+			bul.bulX = -100;
+			bul.bulY = -100;
+		}
+		if (E_getpos2 < bul2.bulR + enemy.EnemyR)
+		{
+			bul2.isbulFlag = 0;
+			enemy.isEnemyAlive = 0;
+			enemy.EnemyX = -200;
+			enemy.EnemyY = -200;
+			Speed = 0;
+			enemycount += 10;
+
+			bul2.bulX = -100;
+			bul2.bulY = -100;
+		}
+		if (E_getpos3 < bul3.bulR + enemy.EnemyR)
+		{
+			bul3.isbulFlag = 0;
+			enemy.isEnemyAlive = 0;
+			enemy.EnemyX = -200;
+			enemy.EnemyY = -200;
+			Speed = 0;
+			enemycount += 10;
+
+			bul3.bulX = -100;
+			bul3.bulY = -100;
+		}
+	}
+	if (enemy.isEnemyAlive == 0)
+	{
+		enemy.enemyTymer = enemy.enemyTymer - 1;
+		if (enemy.enemyTymer < 0)
+		{
+			enemy.isEnemyAlive = 1;
+			enemy.enemyTymer = 120;
+			enemy.EnemyX = rand() % 200 + 100;
+			enemy.EnemyY = 10;
+			if (rand() % 2)
+			{
+				Speed = 2;
+			}
+			else
+			{
+				Speed = -2;
+			}
+
+		}
+	}
+	E_getpos4 = sqrt((enemy2.EnemyX - bul.bulX) * (enemy2.EnemyX - bul.bulX) + (enemy2.EnemyY - bul.bulY) * (enemy2.EnemyY - bul.bulY));
+	E_getpos5 = sqrt((enemy2.EnemyX - bul2.bulX) * (enemy2.EnemyX - bul2.bulX) + (enemy2.EnemyY - bul2.bulY) * (enemy2.EnemyY - bul2.bulY));
+	E_getpos6 = sqrt((enemy2.EnemyX - bul3.bulX) * (enemy2.EnemyX - bul3.bulX) + (enemy2.EnemyY - bul3.bulY) * (enemy2.EnemyY - bul3.bulY));
+	if (bul.isbulFlag == 1 || bul2.isbulFlag == 1 || bul3.isbulFlag == 1 || enemy2.isEnemyAlive == 1)
+	{
+		if (E_getpos4 < bul.bulR + enemy2.EnemyR)
+		{
+			bul.isbulFlag = 0;
+			enemy2.isEnemyAlive = 0;
+			enemy2.EnemyX = -200;
+			enemy2.EnemyY = -200;
+			Speed2 = 0;
+			enemycount += 15;
+
+			bul.bulX = -100;
+			bul.bulY = -100;
+		}
+		if (E_getpos5 < bul2.bulR + enemy2.EnemyR)
+		{
+			bul2.isbulFlag = 0;
+			enemy2.isEnemyAlive = 0;
+			enemy2.EnemyX = -200;
+			enemy2.EnemyY = -200;
+			Speed2 = 0;
+			enemycount += 15;
+
+			bul2.bulX = -100;
+			bul2.bulY = -100;
+		}
+		if (E_getpos6 < bul3.bulR + enemy2.EnemyR)
+		{
+			bul3.isbulFlag = 0;
+			enemy2.isEnemyAlive = 0;
+			enemy2.EnemyX = -200;
+			enemy2.EnemyY = -200;
+			Speed2 = 0;
+			enemycount += 15;
+
+			bul3.bulX = -100;
+			bul3.bulY = -100;
+		}
+	}
+	if (enemy2.isEnemyAlive == 0)
+	{
+		enemy2.enemyTymer = enemy2.enemyTymer - 1;
+		if (enemy2.enemyTymer < 0)
+		{
+			enemy2.isEnemyAlive = 1;
+			enemy2.enemyTymer = 120;
+			enemy2.EnemyX = rand() % 200 + 100;
+			enemy2.EnemyY = 100;
+			if (rand() % 2)
+			{
+				Speed2 = 2;
+			}
+			else
+			{
+				Speed2 = -2;
+			}
+		}
+	}
+}
 void Initialize()
 {
 	play.PlayerX = 250, play.PlayerY = 250;
 	enemy.EnemyX = 10, enemy.EnemyY = 10, enemy.EnemyR = 18, enemy.isEnemyAlive = 1, enemy.enemyTymer = 120;
 	enemy2.EnemyX = 500, enemy2.EnemyY = 100, enemy2.EnemyR = 16, enemy2.isEnemyAlive = 1, enemy2.enemyTymer = 120;
+	bul.bulX = -10, bul.bulY = -10, bul.bulR = 10, bul.isbulFlag = 0;
+	bul2.bulX = -10, bul2.bulY = -10, bul2.bulR = 10, bul2.isbulFlag = 0;
+	bul3.bulX = -10, bul3.bulY = -10, bul3.bulR = 10, bul3.isbulFlag = 0;
+
 
 }
